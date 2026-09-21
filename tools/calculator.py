9347f0e0b0a0d20faceb8e7420d5d9dd1680dd37
AnggaSkibidi <anggasubekti996@gmail.com>
2026-09-21 22:24:02 +0700

    feat: scaffold calculator tool

---
+++ tools/calculator.py
@@ -0,0 +1,9 @@
+"""Calculator tool placeholder."""
+
+
+def calculate(expression: str):
+    """Evaluate a calculator expression.
+
+    Implementation will be added in the calculator milestone.
+    """
+    raise NotImplementedError
